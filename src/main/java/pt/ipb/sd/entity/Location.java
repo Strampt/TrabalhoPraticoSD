55a78f74aa4c739747e05b3c5d1f2e1ee3135b53
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-19 11:41:53 +0000

    Location entity done

---
+++ src/main/java/pt/ipb/sd/entity/Location.java
@@ -0,0 +1,64 @@
+package pt.ipb.sd.entity;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NamedQueries({@NamedQuery(name = Location.COUNT_ALL, query = "SELECT COUNT(b) FROM Location b"),
+        @NamedQuery(name = Location.ALL, query = "SELECT b FROM Location b"),
+        @NamedQuery(name = Location.BY_CITY, query = "SELECT b FROM Location b WHERE b.city = :city"),
+        @NamedQuery(name = Location.COUNT_BY_CITY, query = "SELECT COUNT(b) FROM Location b WHERE b.city = :city")})
+
+public class Location implements Serializable {
+    public final static String ALL = "pt.ipb.sd.jpa.entity.Location.ALL";
+    public final static String COUNT_ALL = "pt.ipb.sd.jpa.entity.Location.COUNT_ALL";
+    public final static String BY_CITY = "pt.ipb.sd.jpa.entity.Location.BY_CITY";
+    public final static String COUNT_BY_CITY = "pt.ipb.sd.jpa.entity.Location.COUNT_BY_CITY";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    String city;
+    String country;
+    String metric;
+
+    public Location(String city, String country, String metric) {
+        this.city = city;
+        this.country = country;
+        this.metric = metric;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+}
